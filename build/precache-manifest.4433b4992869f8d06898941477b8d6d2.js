27b217494650601da017193d9074fa3bc480c2f9
eric li <ericbli.se@gmail.com>
2020-03-30 16:09:33 +0200

    init

---
+++ build/precache-manifest.4433b4992869f8d06898941477b8d6d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb07c8ff2c37e1ff709a5c00ba55f140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec6eeb3ef623f2462d36",
+    "url": "/static/css/main.b28d2842.chunk.css"
+  },
+  {
+    "revision": "dd84f3db4ac407200b07",
+    "url": "/static/js/2.50364907.chunk.js"
+  },
+  {
+    "revision": "ec6eeb3ef623f2462d36",
+    "url": "/static/js/main.be1bb899.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
